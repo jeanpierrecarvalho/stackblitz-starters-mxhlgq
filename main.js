11ed1478e02add559419f6e8d60fa518679c3d21
jeanpierrecarvalho <hello@jeanpierrecarvalho.pt>
2023-12-07 22:02:51 +0000

    teste

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-[...document.querySelectorAll('.product-card')].map((card) => { card.style.backgroundColor = "red" })
-console.log("teste", [...document.querySelectorAll('.product-card')])
-//sdfsdf
+[...document.querySelectorAll('.product-card')].map((card) => {
+    card.style.border = "2px solid #3C4196"
+    card.style.backgroundColor = "#F7F7F9"
+    card.style.borderRadius = "8px"
+    
+    const content = card.querySelector('.product-card__content')
+    content.style.padding = "0px 12px 10px 12px"
+    
+    const picture = card.querySelector('.product-card-carousel .carousel-container')
+    if(picture) picture.style.borderRadius = "0px"
+})
