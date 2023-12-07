b5d123e09bc193212e0b52caeabc39225759d32b
jeanpierrecarvalho <hello@jeanpierrecarvalho.pt>
2023-12-07 21:38:51 +0000

    sdfsdfsdf

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 [...document.querySelectorAll('.product-card')].map((card) => { card.style.backgroundColor = "red" })
 console.log("teste", [...document.querySelectorAll('.product-card')])
+//sdfsdf
